6cb92b0e7c4f20f7ff9347ea616830ac3d8b6bc6
codeinus59 <alexvad1488@gmail.com>
2017-02-01 22:08:42 +0500

    Zadachi 2,3

---
+++ ZadachiYaziky/Matrix.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadachiYaziky
+{
+    class Matrix
+    {
+
+        static private Random r = new Random();
+        int n;
+        /*Основные матрицы*/
+        int[,] a = new int[40, 40];
+        int[,] b = new int[40, 40];
+        int[,] c = new int[40, 40];
+        int[,] d = new int[40, 40];
+
+        /*Вспомогательные*/
+        int[,] h1 = new int[80, 80];
+        int[,] h2 = new int[80, 80];
+        int[,] h3 = new int[80, 80];
+        int[,] h4 = new int[80, 80];
+
+        public void input()
+        {
+            Console.WriteLine("Размерность матриц?");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = r.Next(20);
+                    b[i, j] = r.Next(20);
+                    c[i, j] = r.Next(20);
+                    d[i, j] = r.Next(20);
+                }
+            
+
+        }
+        public void Transpon()
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    a[i, j] ^= a[j, i];
+                    a[j, i] ^= a[i, j];
+                    a[i, j] ^= a[j, i];
+                }
+            }
+        }
+
+        public void multMatrix()
+        {
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    h1[i,j] = 0;
+                    for (int k = 0; k < n; k++)
+                        h1[i,j] += c[i,k] * d[k,j];
+                }
+            
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    h2[i, j] = 0;
+                    for (int k = 0; k < n; k++)
+                        h2[i, j] += b[i, k] * h1[k, j];
+                }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    h3[i, j] = 0;
+                    for (int k = 0; k < n; k++)
+                        h3[i, j] += a[i, k] * h2[k, j];
+                }
+               
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    h3[i, j] = h3[i, j] * 4;
+                }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    d[i, j] = d[i, j] * 3;
+                }
+
+        }
+
+        public void minus()
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    h4[i, j] = h1[i, j] - h3[i, j];
+                }
+        }
+
+        public void outp()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0, 5}", h4[i, j]);
+                }
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ ZadachiYaziky/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadachiYaziky
+{
+    class Magic
+    {
+        static int n, m, sum, sumCol, sumRow;
+        static int[,] a = new int[40, 40];
+        static bool IsCheck;
+
+        public void inp()
+        {
+            int i, j;
+            Console.WriteLine("Размерность?");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числа");
+            for (i = 0; i < n; i++)
+                for (j = 0; j < n; j++)
+                    a[i, j] = Convert.ToInt32(Console.ReadLine());
+
+            i = 1; j = 1;
+            sum = 0;
+            for (int g = 0; g < n; g++)
+            {
+                for (int s = 0; s < n; s++)
+                {
+                    m = a[s, g];
+                    sum = sum + m;
+
+
+                }
+                sum = 0;
+                if (g > 0 && sumRow == sum || g == 0)
+                {
+                    sumRow = sum;
+                }
+                else if (g > 0 && sumRow != sum)
+                {
+                    Console.WriteLine("Суммы строк {0} и {1} не равны. Квадрат не магический.", g - 1, g);
+                    IsCheck = false;
+                    break;
+                }
+                IsCheck = true;
+            }
+            if (IsCheck)
+            {
+                sum = 0;
+                for (int s = 0; s < n; s++)
+                {
+                    for (int g = 0; g < n; g++)
+                    {
+                        m = a[g, s];
+                        sum = sum + m;
+
+
+                    }
+
+                    if (s > 0 && sumCol == sum || s == 0)
+                    {
+                        sumCol = sum;
+                    }
+                    else if (s > 0 && sumCol != sum)
+                    {
+                        Console.WriteLine("Суммы столбцов {0} и {1} не равны. Матрица не магическая.", s - 1, s);
+                        IsCheck = false;
+                        break;
+                    }
+                    sum = 0;
+                }
+            }
+            if (IsCheck)
+            {
+                Console.WriteLine("Суммы строк и столбцов равны. Матрица магическая!");
+            }
+
+
+
+        }
+
+        public void outp()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < n; j++)
+                    Console.Write("{0, 5}", a[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            /*Magic matrix = new Magic();
+            matrix.inp();
+            matrix.outp();*/
+            Matrix mat = new Matrix();
+            mat.input();
+            mat.outp();
+            mat.Transpon();
+            mat.multMatrix();
+            mat.minus();
+            mat.outp();
+            Console.ReadKey();
+        }
+    }
+}
